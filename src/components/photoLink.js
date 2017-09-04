b8990eb87a01899bebc5d14c1ff12348951e115e
Jonathan Hemnes <jonathan.hemnes@talentkode.com>
2017-09-04 16:55:33 -0600

    got photoLink working

---
+++ src/components/photoLink.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { css } from 'glamor'
+import Link from 'gatsby-link'
+import PropTypes from 'prop-types'
+
+const container = css({
+    position: `relative`
+})
+
+const text = css({
+    position: `absolute`,
+    textAlign: `center`,
+    top: `50%`,
+    left: `50%`,
+    transform: `translate(-50%, -50%)`
+})
+
+
+const PhotoLink = (props) => (
+    <div {...container}>
+        <Link to={props.to}>
+            <img src={props.src} />
+            <div {...text}>
+                <h3>{props.text}</h3>
+                <h4>{props.subText}</h4>
+            </div>
+        </Link>
+    </div>
+)
+
+PhotoLink.PropTypes = {
+    to: PropTypes.string.isRequired,
+    src: PropTypes.string.isRequired,
+    text: PropTypes.string,
+    subText: PropTypes.string
+}
+
+export default PhotoLink
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,31 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import { css } from 'glamor'
 import Splash from '../components/splash'
 import mainSplashLogo from '../img/header/MainPageHeader.jpg'
+import headerPhoto from '../img/PhotographyHeader2-TheUnfoundDoor.jpg'
 import WhatWeDo from '../components/whatWeDo'
+import PhotoLink from '../components/photolink'
 
-const IndexPage = ({ data }) =>(
-    <div>
-      <Splash src={mainSplashLogo} />
-      <WhatWeDo email={data.site.siteMetadata.mainEmail} />
+const photoArray = css({
+  display: `flex`,
+  flexDirection: `row`
+})
+
+const IndexPage = ({ data }) => (
+  <div>
+    <Splash src={mainSplashLogo} />
+    <WhatWeDo email={data.site.siteMetadata.mainEmail} />
+    <div {...photoArray}>
+      <PhotoLink to={'/'} src={headerPhoto} text={'Photography'} />
+      <PhotoLink to={'/'} src={headerPhoto}  text={'Videography'} />
+      <PhotoLink to={'/'} src={headerPhoto}  text={'Design & Strategy'} />
     </div>
-  )
+    <div {...photoArray}>
+      <PhotoLink to={'/'} src={headerPhoto} text={'Who We Are'} subText={'Artists, Innovators, Professionals'} />
+    </div>
+  </div>
+)
 
 export default IndexPage
 
