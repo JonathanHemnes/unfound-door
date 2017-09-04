7cbca802011bf7a2f5dcb21813737e5d45d8ff59
Jonathan Hemnes <jonathan.hemnes@talentkode.com>
2017-09-04 13:27:42 -0600

    killed scss

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 import Header from '../components/Header'
+import { css } from 'glamor'
 
 
 const TemplateWrapper = ({ children }) => (
