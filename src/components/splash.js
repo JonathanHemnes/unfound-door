afe2dc22d7d3b7ff3594cd8012d6869647198b94
Jonathan Hemnes <jonathan.hemnes@talentkode.com>
2017-09-04 14:29:44 -0600

    better splash stying

--- src/components/splash.js
+++ src/components/splash.js
@@ -3,18 +3,25 @@ import PropTypes from 'prop-types'
 import { css } from 'glamor'
 import StyleStandards from '../styleStandards'
 
-const splashStyle = css({
-    width: `100vw`
+const imgWrapper = css({
+    width: `100vw`,
+    maxHeight: `100vh`,
+    overflow: `hidden`
+})
+
+const imgStyle = css({
+    width: `100%`,
 })
 
 const Splash = props => (
-    <div>
-        <img src={props.src} {...splashStyle}/>
+    <div {...imgWrapper}>
+        <img src={props.src} alt={props.alt} {...imgStyle}/>
     </div>
 )
 
 Splash.propTypes = {
-  src: PropTypes.string.isRequired
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string
 }
 
 export default Splash
