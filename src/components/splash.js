8d53013e04df26692c1ce0e212efec5703f1f509
Jonathan Hemnes <jonathan.hemnes@talentkode.com>
2017-09-04 14:01:24 -0600

    added splash

---
+++ src/components/splash.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { css } from 'glamor'
+import StyleStandards from '../styleStandards'
+
+const Splash = props => (
+    <div>
+        this is splash
+    </div>
+)
+
+export default Splash
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Splash from '../components/splash'
 
 const IndexPage = () => (
   <div>
-    Hi people
+    <Splash />
+    <div>
+      Hi people
+  </div>
   </div>
 )
 
