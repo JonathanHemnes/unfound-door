b57411449521536091addcb82e643d6b8b5dc38b
Jonathan Hemnes <jonathan.hemnes@talentkode.com>
2017-09-10 17:31:19 -0600

    using logo

--- src/components/address.js
+++ src/components/address.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { css } from 'glamor'
+import logo from '../img/TUD_logo_black.png'
 
 const Address = () => (
     <div>
-        <h2>The Unfound Door</h2>
+        <img src={logo} />
         <p>Denver, CO</p>
         <a href={'mailto:ely@theunfounddoor.com'}>ely@theunfounddoor.com</a>
         <a href={'tel:6177759911'}>617-775-9911</a>
