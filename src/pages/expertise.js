67272ad62369d0b5a09cbc52958ad80e83519009
Jonathan Hemnes <jonathan.hemnes@talentkode.com>
2017-09-10 13:21:56 -0600

    cleanup

--- src/pages/expertise.js
+++ src/pages/expertise.js
@@ -3,12 +3,13 @@ import { css } from 'glamor'
 import Link from 'gatsby-link'
 import Splash from '../components/splash'
 import expertiseLogo from '../img/header/expertise.jpg'
+import StrategicPartnership from '../components/strategicPartnership'
 
 const Expertise = props => (
     <div>
     <Splash src={expertiseLogo} />
     <div>
-      this is the expertise page
+      <StrategicPartnership />
   </div>
   </div>
 )
