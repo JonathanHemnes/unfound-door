b83c9b794b275f5a6e80d9be7df2edcc5982134f
Jonathan Hemnes <jonathan.hemnes@talentkode.com>
2018-01-07 15:28:30 -0700

    added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,24 @@ module.exports = {
                 name: `src`,
                 path: `${__dirname}/src/`,
             },
+        },
+        {
+            resolve: `gatsby-plugin-favicon`,
+            options: {
+                logo: "./src/favicon.png",
+                injectHTML: true,
+                icons: {
+                    android: true,
+                    appleIcon: true,
+                    appleStartup: true,
+                    coast: false,
+                    favicons: true,
+                    firefox: true,
+                    twitter: false,
+                    yandex: false,
+                    windows: false
+                }
+            }
         }
     ]
 }
