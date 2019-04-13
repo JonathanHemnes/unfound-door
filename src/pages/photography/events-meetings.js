be0aafb2aa26bac08b64c16499fb06e3a0e71733
Jonathan Hemnes <jhemnes+github@gmail.com>
2019-04-13 09:16:22 -0600

    removing unnecessary processing

--- src/pages/photography/events-meetings.js
+++ src/pages/photography/events-meetings.js
@@ -22,7 +22,7 @@ query ImagesQuery {
         node {
             childImageSharp {
                 fixed(width:600) {
-                    ...GatsbyImageSharpFixed_tracedSVG
+                    ...GatsbyImageSharpFixed
                 }
             }
         }
